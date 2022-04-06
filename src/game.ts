cf88006f2c9f562db83c0d26249a20728fe4c8a7
shocker <fmju96@gmail.com>
2022-04-06 15:06:25 -0400

    Monopoly game base

---
+++ src/game.ts
@@ -0,0 +1,78 @@
+import Player from './player';
+
+interface GameResponse {
+  success: boolean,
+  message: string,
+}
+
+const systemMessages = {
+  // init
+  'already init': 'The game is already initialized',
+  'init success': 'The game was initialized succesfully',
+
+  // join
+  'join success': (playerNick: string) => `${playerNick} joined the game`,
+  'already joined': (playerNick: string) => `${playerNick} is already in the game`,
+  'game full': 'The game is full',
+
+  // leave
+  'not in game': (playerNick: string) => `${playerNick} is not in this game`,
+};
+
+export default class Game {
+  players: Array<Player> = [];
+  state: 'creating' | 'running' | 'finished' = 'creating';
+
+  response(success: boolean, message: string): GameResponse {
+    return { success, message };
+  }
+
+  /**
+   * Initialize the game after all players have joined
+   * @returns A proper response
+   */
+  initialize(): GameResponse {
+    if (this.state !== 'creating') {
+      return this.response(false, systemMessages['already init']);
+    }
+
+    this.state = 'running';
+    return this.response(true, systemMessages['init success']);
+  }
+
+  /**
+   * Join a game that is not yet initialized
+   * @param player the player who tries to join
+   * @returns A proper response
+   */
+  join(player: Player): GameResponse {
+    if (this.state !== 'creating') {
+      return this.response(false, systemMessages['already init']);
+    }
+
+    if (this.players.includes(player)) {
+      return this.response(false, systemMessages['already joined'](player.nick));
+    }
+
+    if (this.players.length > 3) {
+      return this.response(false, systemMessages['game full']);
+    }
+
+    this.players.push(player);
+    return this.response(true, systemMessages['join success'](player.nick));
+  }
+
+  leave(player: Player): GameResponse {
+    if (this.state !== 'creating') {
+      return this.response(false, systemMessages['already init']);
+    }
+
+    if (!this.players.includes(player)) {
+      return this.response(false, systemMessages['not in game'](player.nick));
+    }
+
+    this.players.splice(this.players.findIndex((element) => element === player), 1);
+    return this.response(true, systemMessages['join success'](player.nick));
+  }
+
+}
---
+++ src/player.ts
@@ -0,0 +1,7 @@
+export default class Player {
+  nick: string;
+
+  constructor(nick: string) {
+    this.nick = nick;
+  }
+}
